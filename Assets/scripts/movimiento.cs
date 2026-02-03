e7f884d33baf48399f5777bc2567893f59ab546f
ElCrazyGeek <elcrazygeeksoyyo@gmail.com>
2026-02-03 11:56:29 -0600

    movimiento basico

---
+++ Assets/scripts/movimiento.cs
@@ -0,0 +1,67 @@
+//using System.Numerics;
+//using Unity.VisualScripting;
+//using System.Numerics;
+using Unity.VisualScripting;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+//using UnityEngine.InputSystem;
+
+public class movimiento : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+       private Rigidbody2D player;
+       private Transform playertrans;
+       public Color playercolor;
+       public SpriteRenderer SR;
+
+       public float movedir;
+       public float movever;
+
+       public float speed;
+       public bool izq;
+       public bool der;
+       public float posx;
+       public float posy;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+        playertrans = GetComponent<Transform>();
+
+   //    playercolor = GetComponent<SpriteRenderer>();
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(izq) /// ponerlo asi activa una casilla en el inspector que si ponemos true o false
+        {
+            player.linearVelocity = new Vector2(-speed, player.linearVelocity.x);
+        } 
+        if(der)
+        {
+            player.linearVelocity = new Vector2(speed, player.linearVelocity.x);
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            player.gravityScale = 1f;
+            playertrans.localScale = new Vector3(0.3f, 0.2f,1);
+       //     SpriteRenderer 
+
+        }
+
+        movedir = Input.GetAxisRaw("Horizontal") * speed; 
+             movever = Input.GetAxisRaw("Vertical") * speed;
+        player.linearVelocity = new Vector2(movedir, movever);
+
+        //guardar la posicion cuando dejes de presiona F
+        //cambiar de color
+
+
+
+    }
+}
