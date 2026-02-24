5a09c25db913653212fb4429ab893376e2f013a4
ElCrazyGeek <elcrazygeeksoyyo@gmail.com>
2026-02-24 11:54:30 -0600

    avance de hoy

---
+++ Assets/Segundo parcial/script/warrior.cs
@@ -0,0 +1,111 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class warrior : MonoBehaviour
+{
+
+    public Animator Walk;
+    public PlayerInput PInput; 
+    public Rigidbody2D RBPlayer;
+
+    public float speed = 2;
+
+    public Vector2 dir;
+    public Vector2 input;
+
+    public bool LookRight;
+
+    public bool canJump;
+
+    public float JumpForce = 5;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+         input = PInput.actions["Caminata"].ReadValue<Vector2>() * speed;
+       dir.x = input.x * speed;
+
+       RBPlayer.linearVelocity = new Vector2(dir.x, RBPlayer.linearVelocity.y);  
+
+
+        input = PInput.actions["Salto"].ReadValue<Vector2>();
+       dir.y = input.y * JumpForce;
+
+       RBPlayer.linearVelocity = new Vector2(RBPlayer.linearVelocity.x, dir.y);  
+
+
+        if (RBPlayer.linearVelocity.x > 0)
+        {
+            LookRight = true;
+        }
+        else if(RBPlayer.linearVelocity.x < 0)
+        {
+            LookRight = false;
+        }
+
+
+        if (LookRight)
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
+         else{
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+        }
+
+
+
+   
+
+        if (RBPlayer.linearVelocity.x != 0)
+        {
+            Walk.SetBool("Is Mooving", true);
+        }
+        else
+        {
+            Walk.SetBool("Is Mooving",false);
+        }
+
+        if()
+
+
+
+/*
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Walk.SetBool("Is Mooving", true);
+
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Walk.SetBool("Is Mooving", false);
+
+        }
+*/
+
+         if (Input.GetKeyDown(KeyCode.F))
+        {
+            Walk.SetBool("Ataque", true);
+        }
+    }
+
+    public void finishiAttack()
+    {
+        Walk.SetBool("Ataque", false);
+    }
+
+    
+}
