da73c8b98ca852f1cf171199cf3c7bd2d29d1978
ElCrazyGeek <elcrazygeeksoyyo@gmail.com>
2026-05-11 09:56:07 -0600

    Pantalla de cierre sin botones funcionales

---
+++ Assets/scripts/Menus/Cerrar juego/Boton.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class Boton : MonoBehaviour
+{
+    [Header("Ajustes")]
+    public float escalaHover = 1.2f;
+    public float duracion = 0.15f;
+    public float escalaClick = 0.9f;
+
+    void Start()
+    {
+        transform.localScale = Vector3.one;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(escalaHover, duracion).SetEase(Ease.OutBack);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(1f, duracion).SetEase(Ease.OutBack);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        transform.DOScale(escalaClick, 0.05f)
+            .SetEase(Ease.InBack)
+            .OnComplete(() => transform.DOScale(1f, 0.1f));
+    }
+}
